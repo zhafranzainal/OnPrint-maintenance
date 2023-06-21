08072f4bbae9c0a4cac49c0b5220c4d78cece96a
nraineyaa <98253793+nraineyaa@users.noreply.github.com>
2023-06-21 21:33:21 +0800

    Commit

--- Views/(4) Payment/paymentgetuserdata.php
+++ Views/(4) Payment/paymentgetuserdata.php
@@ -1,41 +1,31 @@
 <?php
+session_start();
 
 function getUsersData($id)
 {
-	$array = array ();
-	$query = mysqli_query("SELECT * FROM 'users' WHERE 'id' =" . $id);
-while($r = mysqli_fetch_assoc($query))
+    $array = array();
+    $query = mysqli_query("SELECT * FROM `users` WHERE `id` =" . $id);
+    while ($r = mysqli_fetch_assoc($query)) {
+        $array['id'] = $r['id'];
+        $array['name'] = $r['name'];
+        $array['mobile_no'] = $r['mobile_no'];
+        $array['address_id'] = $r['address_id'];
+    }
 
-
-{
-		$array['id'] = $r['id'];
-		$array['name'] = $r['name'];
-		$array['mobile_no'] = $r['mobile_no'];
-		$array['address_id'] = $r['address_id'];
+    return $array;
 }
 
-	return $array;
-}
-
-function getid ($username)
+function getid($username)
 {
-    $query = $query = mysqli_query("SELECT 'id' FROM 'users' WHERE 'username' =". $username);
-    while($r = mysqli_fetch_assoc($query))
-
-    {
+    $query = mysqli_query("SELECT `id` FROM `users` WHERE `username` ='" . $username . "'");
+    while ($r = mysqli_fetch_assoc($query)) {
         return $r['id'];
     }
 }
 
-?>
-
-<?php
-//display the userdata
-
-if (isset ($_SESSION['username']))
-{
-    $userdata = getUserData(getid($_SESSION['username']));
+// Display the userdata
+if (isset($_SESSION['username'])) {
+    $userdata = getUsersData(getid($_SESSION['username']));
     ?>
-    <?php echo $userdata ['username']
-
-}
+    <?php echo $userdata['username']; ?>
+<?php } ?>
